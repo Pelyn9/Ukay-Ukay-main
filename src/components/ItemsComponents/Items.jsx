2a527eedf1b246b32b77946f0c4615fb16b2cb39
Pelyn9 <130518653+Pelyn9@users.noreply.github.com>
2024-10-13 14:31:28 +0800

    okay

--- src/components/ItemsComponents/Items.jsx
+++ src/components/ItemsComponents/Items.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import ProductList from "./ProductList";  // Ensure this is correctly imported
 import Footer from "./Footer";
 import styles from "./App.module.css";
+import SearchBar from "../ItemsComponents/SearchBar";
 
 const Items = () => {
   return (
     <div className={styles.app}>
       <Header />
+      <SearchBar />
       <ProductList />  {/* Your product list */}
       <Footer />
     </div>
